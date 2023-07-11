7444256e4c52492a473ee84b8f4c32bd42f50f56
xixiha <xixiha5230@gmail.com>
2023-07-11 10:03:57 +0800

    fix shape error

--- algorithm/ActorCritic.py
+++ algorithm/ActorCritic.py
@@ -14,15 +14,15 @@ from utils.ConfigHelper import ConfigHelper
 
 
 class ActorCritic(nn.Module):
-    ''' Actor Critic Module '''
+    """Actor Critic Module"""
 
     def __init__(self, obs_space, action_space, config: ConfigHelper):
-        '''
+        """
         Args:
             obs_space {tuple} -- observation space
             action_space {tuple} -- action space
             config {dict} -- config dictionary
-        '''
+        """
         super(ActorCritic, self).__init__()
         self.hidden_layer_size = config.hidden_layer_size
         self.multi_task = config.multi_task
@@ -31,9 +31,21 @@ class ActorCritic(nn.Module):
         self.obs_space = obs_space
 
         # Observation feature extraction
-        if isinstance(obs_space, (gym_spaces.Tuple, gymnasium_spaces.Tuple)) and obs_space[0].shape == (84, 84, 3) and obs_space[1].shape == (400,):
-            # MT UGV
+        if (
+            isinstance(obs_space, (gym_spaces.Tuple, gymnasium_spaces.Tuple))
+            and len(obs_space) > 2
+            and obs_space[0].shape == (84, 84, 3)
+            and obs_space[1].shape == (400,)
+        ):
+            # UGV
             self.obs_net = ObsNetUGV(obs_space)
+        elif (
+            isinstance(obs_space, (gym_spaces.Tuple, gymnasium_spaces.Tuple))
+            and len(obs_space) == 1
+            and obs_space[0].shape == (84, 84, 3)
+        ):
+            # UGV single image
+            self.obs_net = ObsNetImage(obs_space[0])
         elif len(obs_space.shape) == 1:
             # MT vector obs
             pass
@@ -42,14 +54,20 @@ class ActorCritic(nn.Module):
             self.obs_net = ObsNetImage(obs_space)
         else:
             raise NotImplementedError(obs_space)
-        in_features_size = self.obs_net.output_size if hasattr(self, 'obs_net') else obs_space.shape[0]
+        in_features_size = (
+            self.obs_net.output_size if hasattr(self, "obs_net") else obs_space.shape[0]
+        )
 
         # RND
         if self.use_rnd:
             # TODO mybe can rnd rnn feature
             # Only rnd the first dimension of the observation space !!!
-            self.rnd = RND(config, obs_space[0].shape if isinstance(
-                obs_space, (gym_spaces.Tuple, gymnasium_spaces.Tuple)) else obs_space.shape)
+            self.rnd = RND(
+                config,
+                obs_space[0].shape
+                if isinstance(obs_space, (gym_spaces.Tuple, gymnasium_spaces.Tuple))
+                else obs_space.shape,
+            )
 
         # Task net
         if self.multi_task:
@@ -57,26 +75,40 @@ class ActorCritic(nn.Module):
             assert self.task_num > 0
             self.task_net = TaskNet(self.obs_space[-1].shape[0], 16)
             self.task_feature_size = self.task_net.output_size
-            self.task_predict_net = TaskPredictNet(self.obs_net.output_size, 128, self.task_num)
+            self.task_predict_net = TaskPredictNet(
+                self.obs_net.output_size, 128, self.task_num
+            )
 
         # RNN
         if self.use_lstm:
             self.rnn_net = RNN(config, in_features_size)
-        after_rnn_size = self.rnn_net.output_size if hasattr(self, 'rnn_net') else in_features_size
+        after_rnn_size = (
+            self.rnn_net.output_size if hasattr(self, "rnn_net") else in_features_size
+        )
 
         # Hidden layer
         self.hidden_net = HiddenNet(after_rnn_size, self.hidden_layer_size)
 
         # Actor Critic
         if self.multi_task:
-            self.actor = MultiGaussianActor(config, self.hidden_layer_size, action_space, self.task_num)
-            self.critic = Critic(config, self.hidden_layer_size + self.task_feature_size, 1)
+            self.actor = MultiGaussianActor(
+                config, self.hidden_layer_size, action_space, self.task_num
+            )
+            self.critic = Critic(
+                config, self.hidden_layer_size + self.task_feature_size, 1
+            )
         else:
             self.actor = GaussianActor(config, self.hidden_layer_size, action_space)
             self.critic = Critic(config, self.hidden_layer_size, 1)
 
-    def forward(self, obs: torch.Tensor, hidden_in: torch.Tensor = None, sequence_length: int = 1, module_index: int = -1):
-        '''
+    def forward(
+        self,
+        obs: torch.Tensor,
+        hidden_in: torch.Tensor = None,
+        sequence_length: int = 1,
+        module_index: int = -1,
+    ):
+        """
         Args:
             obs {tensor, list} -- observation tensor
             hidden_in {torch.Tensor} -- RNN hidden in feature
@@ -88,7 +120,7 @@ class ActorCritic(nn.Module):
             {tensor}: rnd value base on current state
             {tensor}: RNN hidden out feature
             {task_predict}: predicted task id
-        '''
+        """
 
         if self.multi_task:
             task_feature = self.task_net(obs[-1])
@@ -96,7 +128,7 @@ class ActorCritic(nn.Module):
         else:
             feature = obs
 
-        feature = self.obs_net(feature) if hasattr(self, 'obs_net') else obs[0]
+        feature = self.obs_net(feature) if hasattr(self, "obs_net") else obs[0]
         obs_feature = feature
 
         if self.use_lstm:
@@ -119,8 +151,14 @@ class ActorCritic(nn.Module):
 
         return dist, value, rnd_value, hidden_out, task_predict
 
-    def eval_forward(self, obs: torch.Tensor, hidden_in: torch.Tensor = None, sequence_length: int = 1, module_index: int = -1):
-        '''
+    def eval_forward(
+        self,
+        obs: torch.Tensor,
+        hidden_in: torch.Tensor = None,
+        sequence_length: int = 1,
+        module_index: int = -1,
+    ):
+        """
         Only use for test no training: auto select actor to forward
         Args:
             obs {tensor, list} -- observation tensor
@@ -129,15 +167,15 @@ class ActorCritic(nn.Module):
             module_index {int} -- index of Actor or Critic to select
         Returns:
             {dist}: action dist
-            {hidden_out}: RNN hidden out feature  
-        '''
+            {hidden_out}: RNN hidden out feature
+        """
 
         if self.multi_task:
             feature = obs[0] if len(obs) <= 2 else obs[:-1]
         else:
             feature = obs
 
-        if hasattr(self, 'obs_net'):
+        if hasattr(self, "obs_net"):
             feature = self.obs_net(feature)
         else:
             if isinstance(feature, list):
@@ -153,7 +191,7 @@ class ActorCritic(nn.Module):
         if self.multi_task:
             task_predict = self.task_predict_net(obs_feature)
             # module_index = torch.argmax(task_predict).item()
-            print(task_predict)
+            # print(task_predict)
             dist = self.actor(feature, task_predict)
         else:
             dist = self.actor(feature)
--- layers/StateNet.py
+++ layers/StateNet.py
@@ -9,8 +9,14 @@ from gymnasium import spaces
 from utils.weights_init import weights_init_
 
 
-def conv1d_output_size(length: int, kernel_size: int = 1, stride: int = 1, padding: int = 0, dilation: int = 1):
-    ''' calculate conv1d output size
+def conv1d_output_size(
+    length: int,
+    kernel_size: int = 1,
+    stride: int = 1,
+    padding: int = 0,
+    dilation: int = 1,
+):
+    """calculate conv1d output size
         comv1d input shape is like (chanel, length)
 
     Args:
@@ -19,8 +25,10 @@ def conv1d_output_size(length: int, kernel_size: int = 1, stride: int = 1, paddi
         stride {int} -- conv1d stride length
         padding {int} -- conv1d padding size
         dilation {int} -- conv1d dilation size
-    '''
-    l_out = floor(((length + (2 * padding) - (dilation * (kernel_size - 1)) - 1) / stride) + 1)
+    """
+    l_out = floor(
+        ((length + (2 * padding) - (dilation * (kernel_size - 1)) - 1) / stride) + 1
+    )
     return l_out
 
 
@@ -31,8 +39,8 @@ def conv2d_output_shape(
     padding: int = 0,
     dilation: int = 1,
 ) -> Tuple[int, int]:
-    '''Calculates the output shape (height and width) of the output of a convolution layer.
-        kernel_size, stride, padding and dilation correspond to the inputs of the 
+    """Calculates the output shape (height and width) of the output of a convolution layer.
+        kernel_size, stride, padding and dilation correspond to the inputs of the
         torch.nn.Conv2d layer (https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html)
 
     Args:
@@ -41,22 +49,26 @@ def conv2d_output_shape(
         stride {int} -- The stride of the convolution
         padding {int} -- The padding of the convolution
         dilation {int} -- The dilation of the convolution
-    '''
+    """
     if not isinstance(kernel_size, tuple):
         kernel_size = (int(kernel_size), int(kernel_size))
-    h = floor(((h_w[0] + (2 * padding) - (dilation * (kernel_size[0] - 1)) - 1) / stride) + 1)
-    w = floor(((h_w[1] + (2 * padding) - (dilation * (kernel_size[1] - 1)) - 1) / stride) + 1)
+    h = floor(
+        ((h_w[0] + (2 * padding) - (dilation * (kernel_size[0] - 1)) - 1) / stride) + 1
+    )
+    w = floor(
+        ((h_w[1] + (2 * padding) - (dilation * (kernel_size[1] - 1)) - 1) / stride) + 1
+    )
     return h, w
 
 
 class AtariImage(nn.Module):
-    ''' image convolution module like Open AI Atari '''
+    """image convolution module like Open AI Atari"""
 
     def __init__(self, shape):
-        '''
+        """
         Args:
             shape {tuple} -- The shape of image like (84, 84, 3)
-        '''
+        """
         super(AtariImage, self).__init__()
         conv_1_hw = conv2d_output_shape((shape[0], shape[1]), 8, 4)
         conv_2_hw = conv2d_output_shape(conv_1_hw, 4, 2)
@@ -73,10 +85,10 @@ class AtariImage(nn.Module):
         self.output_size = conv_3_hw[0] * conv_3_hw[1] * 64
 
     def forward(self, x: torch.Tensor):
-        '''
+        """
         Args:
             x {torch.Tensor} -- image tensor shape like (84, 84, 3)
-        '''
+        """
         if len(x.shape) == 3:
             x = x.unsqueeze(0)
         x = x.permute(0, 3, 1, 2)
@@ -86,15 +98,15 @@ class AtariImage(nn.Module):
 
 
 class Conv1d(nn.Module):
-    ''' dense sensor data convolution module '''
+    """dense sensor data convolution module"""
 
     def __init__(self, length: int, channel: int, output_size: int):
-        '''
+        """
         Args:
             length {int} -- data length
             channel {int} -- data chanel
             output_size {int} -- output dimension
-        '''
+        """
         super(Conv1d, self).__init__()
 
         conv_1_l = conv1d_output_size(length, 8, 4)
@@ -108,18 +120,15 @@ class Conv1d(nn.Module):
         self.conv.apply(weights_init_)
 
         self.fc_input = conv_2_l * 32
-        self.fc = nn.Sequential(
-            nn.Linear(self.fc_input, output_size),
-            nn.ReLU()
-        )
+        self.fc = nn.Sequential(nn.Linear(self.fc_input, output_size), nn.ReLU())
         self.fc.apply(weights_init_)
         self.output_size = output_size
 
     def forward(self, x: torch.Tensor):
-        '''
+        """
         Args:
             x {torch.Tensor} -- tensor of sensor data shape like (batch, 400)
-        '''
+        """
         batch = x.shape[0]
         if len(x.shape) == 2:
             x = x.unsqueeze(1)
@@ -127,57 +136,59 @@ class Conv1d(nn.Module):
         hidden = hidden.view(batch, -1)
         x = self.fc(hidden)
         return x
-    
+
+
 class ObsNetImage(nn.Module):
-    ''' single image process module '''
+    """single image process module"""
 
     def __init__(self, obs_shape: spaces.Tuple, output_size: int = 128) -> None:
-        '''
+        """
         Args:
             obs_shape {tuple} -- like (84, 84, 3)
-        '''
-        assert obs_shape == (84, 84, 3) or obs_shape == (96, 96, 3)
+        """
+        assert obs_shape.shape == (84, 84, 3) or obs_shape.shape == (96, 96, 3)
         super(ObsNetImage, self).__init__()
-        self.conv2d = AtariImage(obs_shape)
+        self.conv2d = AtariImage(obs_shape.shape)
         self.fc = nn.Sequential(
-            nn.Linear(self.conv2d.output_size, output_size),
-            nn.ReLU()
+            nn.Linear(self.conv2d.output_size, output_size), nn.ReLU()
         )
         self.fc.apply(weights_init_)
         self.output_size = output_size
 
     def forward(self, obs: torch.Tensor):
-        '''
+        """
         Args:
-            obs {torch.Tensor} -- tensor of image 
-        '''
+            obs {torch.Tensor} -- tensor of image
+        """
+        obs = obs[0]
         if len(obs.shape) == 3:
             obs = obs.unsqueeze(0)
         x = self.conv2d(obs)
         x = self.fc(x)
         return x
-    
+
+
 class ObsNetUGV(nn.Module):
-    ''' UGV environment data process module '''
+    """UGV environment data process module"""
 
     def __init__(self, obs_space: spaces.Tuple) -> None:
-        '''
+        """
         Args:
             obs_space {spaces.Tuple} -- shape is ((84, 84, 3), (400, ))
-        '''
+        """
         assert obs_space[0].shape == (84, 84, 3)
         assert obs_space[1].shape == (400,)
         super(ObsNetUGV, self).__init__()
 
         self.conv1d = Conv1d(obs_space[1].shape[0], 1, 64)
-        self.conv2d = ObsNetImage(obs_space[0].shape, 64)
+        self.conv2d = ObsNetImage(obs_space[0], 64)
         self.output_size = self.conv1d.output_size + self.conv2d.output_size
 
     def forward(self, obs: list):
-        '''
+        """
         Args:
             obs {list} -- state is list of [image tensor, sensor tensor]
-        '''
+        """
         img_batch = obs[0]
         ray_batch = obs[1]
 
@@ -186,4 +197,3 @@ class ObsNetUGV(nn.Module):
 
         x = torch.cat([img, ray], dim=-1)
         return x
-    
