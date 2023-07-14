08a4668e23441c3a2bdfe757e5a490ae59e9939d
xixiha <xixiha5230@gmail.com>
2023-07-14 09:20:02 +0800

    new format

--- test.py
+++ test.py
@@ -14,28 +14,19 @@ from utils.obs_2_tensor import _obs_2_tensor
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--config_file',
+    "--config_file",
     type=str,
-    default='PPO_logs/CarRace/new_env/run_0/config.yaml',
-    help='The config file',
+    default="PPO_logs/UnityMultitask/gru_test_auto_predict_obs/run_0/configfinal.yaml",
+    help="The config file",
 )
 parser.add_argument(
-    '--ckpt',
+    "--ckpt",
     type=int,
     default=0,
-    help='The checkpoint index',
+    help="The checkpoint index",
 )
-parser.add_argument(
-    '--rm_rnn',
-    type=int,
-    default=0,
-    help='The checkpoint index',
-)
-parser.add_argument(
-    '--save_gif',
-    type=bool,
-    default=False
-)
+
+parser.add_argument("--save_gif", type=bool, default=False)
 
 
 def test(args):
@@ -48,30 +39,34 @@ def test(args):
     save_gif = args.save_gif
     custom_ckpt = args.ckpt
 
-    env = create_env(config, render_mode='rgb_array' if save_gif else 'human', id=51, time_scale=1)
+    env = create_env(
+        config, render_mode="rgb_array" if save_gif else "human", id=51, time_scale=1
+    )
     observation_space = env.observation_space
-    if config.action_type == 'continuous':
+    if config.action_type == "continuous":
         action_space = env.action_space
-    elif config.action_type == 'discrete':
+    elif config.action_type == "discrete":
         action_space = env.action_space.n
     if config.use_state_normailzation:
-        state_normalizer = logger.load_pickle('state_normalizer.pkl')
+        state_normalizer = logger.load_pickle("state_normalizer.pkl")
 
     # initialize a PPO agent
     ppo_agent = PPO(observation_space, action_space, config)
-    ppo_agent.policy.eval()
 
     # load latest checkpoint
-    latest_checkpoint = max(glob.glob(os.path.join(logger.checkpoint_path, '*')), key=os.path.getctime)
+    latest_checkpoint = max(
+        glob.glob(os.path.join(logger.checkpoint_path, "*")), key=os.path.getctime
+    )
     if custom_ckpt != 0:
-        latest_checkpoint = f'{logger.checkpoint_path}/{custom_ckpt}.pth'
-    print(f'resume from {latest_checkpoint}')
+        latest_checkpoint = f"{logger.checkpoint_path}/{custom_ckpt}.pth"
+    print(f"resume from {latest_checkpoint}")
     ppo_agent.load(latest_checkpoint)
+
     # start testing
     test_running_reward = 0
     images = []
     with torch.no_grad():
-        for ep in range(1, total_test_episodes+1):
+        for ep in range(1, total_test_episodes + 1):
             state = env.reset()
             if config.use_state_normailzation:
                 if config.multi_task:
@@ -82,36 +77,42 @@ def test(args):
             step = 0
             while True:
                 h_in = h_out
-                action, h_out = ppo_agent.eval_select_action(_obs_2_tensor(state, config.device), h_in)
+                action, h_out, task_predict = ppo_agent.eval_select_action(
+                    _obs_2_tensor(state, config.device), h_in
+                )
+                # imageio.imwrite(
+                #     f"./assert/{ep}_image_{step}_{task_predict.cpu().numpy()[0]}.png",
+                #     state[0],
+                # )
                 step += 1
-                if(args.rm_rnn != 0 and step % args.rm_rnn == 0):
-                    h_out = ppo_agent.init_recurrent_cell_states(1)
                 state, _, _, info = env.step(action[0].cpu().numpy())
                 if config.use_state_normailzation:
                     if config.multi_task:
                         state_normalizer(state[:-1]).append(state[-1])
                     else:
                         state = state_normalizer(state)
-
                 env.render()
-                # pygame.event.get()
                 if save_gif:
                     images.append(env.render())
                 time.sleep(0.01)
                 if info:
                     print(f'Episode: {ep} \t\t Reward: {info["reward"]}')
-                    test_running_reward += info['reward']
+                    test_running_reward += info["reward"]
                     break
 
     if save_gif:
-        imageio.mimsave(f'{logger.run_log_dir}/test.gif', images)
+        imageio.mimsave(f"{logger.run_log_dir}/test.gif", images)
     env.close()
 
-    print('============================================================================================')
+    print(
+        "============================================================================================"
+    )
     avg_test_reward = test_running_reward / total_test_episodes
-    print('average test reward : ' + str(avg_test_reward))
-    print('============================================================================================')
+    print("average test reward : " + str(avg_test_reward))
+    print(
+        "============================================================================================"
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test(parser.parse_args())
--- train.py
+++ train.py
@@ -4,17 +4,17 @@ from trainer.Trainer import Trainer
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--config_file',
+    "--config_file",
     type=str,
     default="configs/UnityMultitask.yaml",
     # default="PPO_logs/UnityMultitask/gru_seq16/run_0/config.yaml",
-    help='The config file',
+    help="The config file",
 )
 
 # TODO
 # 任务表征网络的必要性
 # 优化整理代码
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parser.parse_args()
     for _ in range(3):
         trainer = Trainer(args.config_file)
