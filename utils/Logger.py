5e7c934ed1c85ea29b17caa8001b25e52fb94d58
xixiha <xixiha5230@gmail.com>
2023-07-20 15:38:59 +0800

    use step not update

--- utils/Logger.py
+++ utils/Logger.py
@@ -28,14 +28,14 @@ class Logger:
         if not test:
             print("save checkpoint path : " + self.checkpoint_path)
 
-    def write_reward(self, update, i_episode, episode_result):
+    def write_reward(self, step, i_episode, episode_result):
         if len(episode_result) > 0:
             self.reward_writter.write(
-                f'{update},\t{i_episode},\t{episode_result["reward_mean"]}\n'
+                f'{step},\t{i_episode},\t{episode_result["reward_mean"]}\n'
             )
             self.reward_writter.flush()
             print(
-                f'update: {update}\t episode: {i_episode}\t reward: {episode_result["reward_mean"]}'
+                f'step: {step}\t episode: {i_episode}\t reward: {episode_result["reward_mean"]}'
             )
 
     @property
@@ -63,7 +63,7 @@ class Logger:
         with open(file, "wb") as f:
             pickle.dump(obj, f)
 
-    def write_tensorboard(self, datas, update):
+    def write_tensorboard(self, datas, step):
         (
             actor_loss_mean,
             critic_loss_mean,
@@ -80,33 +80,33 @@ class Logger:
             mean_rnd_reward,
         ) = datas
 
-        self.writer.add_scalar("Loss/actor", np.mean(actor_loss_mean), update)
-        self.writer.add_scalar("Loss/critic", np.mean(critic_loss_mean), update)
-        self.writer.add_scalar("Loss/total", np.mean(total_loss_mean), update)
-        self.writer.add_scalar("Loss/entropy", np.mean(dist_entropy_mean), update)
+        self.writer.add_scalar("Loss/actor", np.mean(actor_loss_mean), step)
+        self.writer.add_scalar("Loss/critic", np.mean(critic_loss_mean), step)
+        self.writer.add_scalar("Loss/total", np.mean(total_loss_mean), step)
+        self.writer.add_scalar("Loss/entropy", np.mean(dist_entropy_mean), step)
         if len(task_loss_mean) != 0:
-            self.writer.add_scalar("Loss/task", np.mean(task_loss_mean), update)
+            self.writer.add_scalar("Loss/task", np.mean(task_loss_mean), step)
         if len(rnd_loss_mean) != 0:
-            self.writer.add_scalar("Loss/rnd", np.mean(rnd_loss_mean), update)
-        self.writer.add_scalar("Parameter/learning_rate", learning_rate, update)
-        self.writer.add_scalar("Parameter/clip_range", clip_range, update)
-        self.writer.add_scalar("Parameter/entropy_coeff", entropy_coeff, update)
-        self.writer.add_scalar("Parameter/task_coeff", task_coeff, update)
+            self.writer.add_scalar("Loss/rnd", np.mean(rnd_loss_mean), step)
+        self.writer.add_scalar("Parameter/learning_rate", learning_rate, step)
+        self.writer.add_scalar("Parameter/clip_range", clip_range, step)
+        self.writer.add_scalar("Parameter/entropy_coeff", entropy_coeff, step)
+        self.writer.add_scalar("Parameter/task_coeff", task_coeff, step)
         if mean_rnd_reward != None:
-            self.writer.add_scalar("Train/rnd_reward_mean", mean_rnd_reward, update)
+            self.writer.add_scalar("Train/rnd_reward_mean", mean_rnd_reward, step)
         if len(episode_result) > 0:
             self.writer.add_scalar(
-                "Train/reward_mean", episode_result["reward_mean"], update
+                "Train/reward_mean", episode_result["reward_mean"], step
             )
-            self.writer.add_scalar("Train/scaled_reward", scaled_rewards, update)
+            self.writer.add_scalar("Train/scaled_reward", scaled_rewards, step)
             self.writer.add_scalar(
-                "Train/reward_std", episode_result["reward_std"], update
-            )
-            self.writer.add_scalar(
-                "Train/length_mean", episode_result["length_mean"], update
+                "Train/reward_std", episode_result["reward_std"], step
             )
             self.writer.add_scalar(
-                "Train/length_std", episode_result["length_std"], update
+                "Train/length_mean", episode_result["length_mean"], step
+            )
+            self.writer.add_scalar(
+                "Train/length_std", episode_result["length_std"], step
             )
 
     def close(self):
